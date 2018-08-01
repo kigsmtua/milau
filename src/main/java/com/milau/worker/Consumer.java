2c54fde93b6183147adea35f33e617267fdc3c0a
John Kiragu <mutuakiragu@gmail.com>
2018-08-01 12:37:56 +0300

    Add new worker model

---
+++ src/main/java/com/milau/worker/Consumer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.milau.worker;
+
+/**
+ *
+ * @author john.kiragu
+ */
+public class Consumer {
+    
+    public void execute(){
+//        Pop stuff from queue and distribute the work
+//        The worker needs to come alongside and use the values that need to come back
+//  
+    }
+}
