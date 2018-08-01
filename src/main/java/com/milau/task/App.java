43289d1b40e111f39815243e8dbb760d700997d7
John Kiragu <mutuakiragu@gmail.com>
2018-08-01 11:27:22 +0300

    Change branch configuration

--- src/main/java/com/milau/task/App.java
+++ src/main/java/com/milau/task/App.java
@@ -10,5 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        System.out.println("Welcome to milau...");
     }
 }
+
