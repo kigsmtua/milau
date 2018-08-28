cd6769c05cc2cb168ef9d6620b0bee27bd051947
John Kiragu <mutuakiragu@gmail.com>
2018-08-28 09:27:41 +0300

    Fix stylistic errors

--- src/main/java/io/github/kigsmtua/milau/worker/Worker.java
+++ src/main/java/io/github/kigsmtua/milau/worker/Worker.java
@@ -23,14 +23,16 @@
  */
 package io.github.kigsmtua.milau.worker;
 
+import java.io.IOException;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.github.kigsmtua.milau.Config;
 import io.github.kigsmtua.milau.task.Task;
-import java.io.IOException;
 import redis.clients.jedis.Jedis;
 
 /**
