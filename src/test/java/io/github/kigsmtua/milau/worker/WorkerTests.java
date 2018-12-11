f2b8d9835f350c45e61156f458154e61ac5d4715
John Kiragu <mutuakiragu@gmail.com>
2018-12-11 14:56:09 +0300

    Appease checkstyle

--- src/test/java/io/github/kigsmtua/milau/worker/WorkerTests.java
+++ src/test/java/io/github/kigsmtua/milau/worker/WorkerTests.java
@@ -1,9 +1,5 @@
 package io.github.kigsmtua.milau.worker;
 
-import io.github.kigsmtua.milau.Config;
-import io.github.kigsmtua.milau.TestActionNonAnnotated;
-import io.github.kigsmtua.milau.TestUtils;
-import io.github.kigsmtua.milau.client.Client;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -13,16 +9,19 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import io.github.kigsmtua.milau.Config;
+import io.github.kigsmtua.milau.TestActionNonAnnotated;
+import io.github.kigsmtua.milau.TestUtils;
+import io.github.kigsmtua.milau.client.Client;
+
+
 import redis.clients.jedis.Jedis;
 
 /**
  * Created by john.kiragu on 19/08/2018.
  */
 public class WorkerTests {
-    /**
-     * What does this configuration even mean
-     * We can use the values as they come alongside here
-     */
+   
     private Client client;
     
     private Jedis jedis;
@@ -34,10 +33,7 @@ public class WorkerTests {
         this.client = new Client(config);
         this.jedis = TestUtils.getJedisConnection(config);
     }
-    /**
-     * Test the jobs are picked from the queue
-     * @throws Exception 
-     */
+    
     @Test
     public void testGetReadyTasksGetsOnlyReadyTasks() throws Exception {
         
@@ -46,29 +42,19 @@ public class WorkerTests {
         jopProperties.put("testActionID", 12333);
         jopProperties.put("someTestData", 23242);
         
-        /**
-         * This job is due now
-         */
         this.client.enqueue("test-queue",  TestActionNonAnnotated.class, 
             jopProperties, 0);
         
-        /**
-         * Enqueue job to done 24hrs from now
-         */
-        
+
         this.client.enqueue("test-queue",  TestActionNonAnnotated.class, 
             jopProperties, 86400000);
         
-        /**
-         * You need to make your methods in such a manner that 
-         */
+ 
         Config config = new Config.ConfigBuilder("127.0.0.1", 6379).build();
         
         
         Worker worker = new Worker(config, queue);
-        /**
-         * Lets 
-         */
+
         Set jobSet = worker.getReadyTasks();
      
         Assert.assertEquals(1, jobSet.size());
